9686c426e50261eac809b837abf0cb55c8bc3f6a
Stephen Fowles-White <FowlesWhite@gmail.com>
2019-06-25 22:45:04 +0100

    Exercise to Learn components

---
+++ Learning comp/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <my-header></my-Header>
+        </div>
+        <hr>
+        <div class="row">
+            <my-server-list></my-server-list>
+            <my-server-details></my-server-details>
+        </div>
+        <hr>
+        <div class="row">
+            <my-footer></my-footer>
+        </div>
+    </div>
+</template>
+
+<script>
+import myHeader from './components/Shared/Header'
+import serverList from './components/Server/ServerList'
+import serverDetails from './components/Server/ServerDetails'
+import footer from './components/Shared/footer'
+
+    export default {
+        components: {
+            'my-header': myHeader,
+            'my-server-list': serverList,
+            'my-server-details': serverDetails,
+            'my-footer': footer
+        }
+
+
+
+    }
+
+</script>
+
+<style>
+
+</style>
