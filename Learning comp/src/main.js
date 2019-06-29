7cf7d9e62b7081284d3817d9b92c3856c453059c
Stephen Fowles-White <FowlesWhite@gmail.com>
2019-06-29 16:29:16 +0100

    Update main.js

--- Learning comp/src/main.js
+++ Learning comp/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+export const eventBus = new Vue({ 
+  methods: { 
+    //method takes an id and status argument to be used globally for the application and enable cross component data rendering
+  methodNameData(id, status){ 
+    this.$emit('statusEdit', {'id': id, serverstatus: status}) 
+   }
+  }
+ })
+
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
